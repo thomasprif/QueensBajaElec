91276a9a47ab19fe70a078e9d66cb756a1d20d84
Aidan <aidanlbaker23@gmail.com>
2026-03-18 13:56:22 -0400

    updates to can freertos library. Added filtering in init and implemented receive callback

--- Libs/can_al_freertos/can_al_freertos.c
+++ Libs/can_al_freertos/can_al_freertos.c
@@ -1,8 +1,10 @@
 #include "can_al_freertos.h"
 
-extern CAN_al_t *can_ptr;
+#include <string.h>
 
-int CAN_al_init(CAN_al_t *can, CAN_al_config *config, uint8_t *filter_ids, int num_of_ids) {
+CAN_al_t *can_ptr;
+
+int CAN_al_init(CAN_al_t *can, CAN_al_config *config, uint16_t *filter_ids, int num_of_ids) {
     can->hcan = config->hcan;
     can->node_id = config->node_id;
     can_ptr = can;
@@ -14,31 +16,78 @@ int CAN_al_init(CAN_al_t *can, CAN_al_config *config, uint8_t *filter_ids, int n
     if (can->rx_queue == NULL || can->tx_queue == NULL) return 1; // Error creating queue
 
     // Setup transmit task
-    // xTaskCreate()?
-
+    if (xTaskCreate(
+        CAN_tx_task,
+        "CAN_tx_task",
+        CAN_TX_TASK_STACK_SIZE,
+        can_ptr,
+        tskIDLE_PRIORITY + 1,
+        NULL
+    ) != pdPASS) return 2;
 
     // Configure the filters
-
+    CAN_Add_Filter_Discrete(can->hcan, num_of_ids, filter_ids);
 
     // Start CAN
     if (HAL_CAN_Start(can->hcan) != HAL_OK) return 3;
 
     if (HAL_CAN_ActivateNotification(can->hcan, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK) return 4;
 
-    return 0; // Success
+    return CAN_OK; // Success
+}
+
+HAL_StatusTypeDef CAN_Add_Filter_Discrete(CAN_HandleTypeDef *hcan, int length, uint16_t *ids) {
+    int ids_left = length;
+
+    CAN_FilterTypeDef CAN_Filter_Config;
+
+    CAN_Filter_Config.FilterActivation = CAN_FILTER_ENABLE;
+    CAN_Filter_Config.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+    CAN_Filter_Config.FilterMode = CAN_FILTERMODE_IDLIST;
+    CAN_Filter_Config.FilterScale = CAN_FILTERSCALE_16BIT;
+
+    for (int i = 0; i < ((length - 1) / 4) + 1; i++) {
+        CAN_Filter_Config.FilterBank = i;
+
+        if (ids_left-- > 0)
+            CAN_Filter_Config.FilterIdHigh = (ids[4 * i] << 5);
+
+        if (ids_left-- > 0)
+            CAN_Filter_Config.FilterIdLow = (ids[4 * i + 1] << 5);
+
+        if (ids_left-- > 0)
+            CAN_Filter_Config.FilterMaskIdHigh = (ids[4 * i + 2] << 5);
+
+        if (ids_left-- > 0)
+            CAN_Filter_Config.FilterMaskIdLow = (ids[4 * i + 3] << 5);
+
+        return HAL_CAN_ConfigFilter(hcan, &CAN_Filter_Config);
+    }
+    return 1;
 }
 
 int CAN_tx_enqueue(CAN_al_t *can, CAN_msg_t *msg) {
-    if (xQueueSend(can->tx_queue, msg, pdMS_TO_TICKS(10)) == pdTRUE) return 0;
+    if (xQueueSend(can->tx_queue, msg, pdMS_TO_TICKS(10)) == pdTRUE) return CAN_OK;
     return 1; // Send queue full
 }
 
 int CAN_rx_get_from_queue(CAN_al_t *can, CAN_msg_t *msg) {
-    if (xQueueReceive(can->rx_queue, msg, portMAX_DELAY) == pdTRUE) return 0;
+    if (xQueueReceive(can->rx_queue, msg, portMAX_DELAY) == pdTRUE) return CAN_OK;
     return 1; //Error retrieving from queue
 }
 
-void HAL_CAN_RxEventCallback(CAN_HandleTypeDef *hcan, uint16_t size) {
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
+    CAN_RxHeaderTypeDef RxHeader;
+    uint8_t data[8];
+    CAN_msg_t msg;
+
+    HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, data);
+
+    msg.id = RxHeader.StdId;
+    msg.len = RxHeader.DLC;
+    memcpy(msg.data, data, msg.len);
+
+    xQueueSendFromISR(can_ptr->rx_queue, &msg, NULL);
 }
 
 void CAN_tx_task(void *pvParameters) {
@@ -52,6 +101,7 @@ void CAN_tx_task(void *pvParameters) {
     TxHeader.RTR = CAN_RTR_DATA;
 
     while (1) {
+        // Check if there is a message in queue to be sent
         if (xQueueReceive(can->tx_queue, &tx_msg, portMAX_DELAY) == pdTRUE) {
             TxHeader.StdId = tx_msg.id;
             TxHeader.DLC = tx_msg.len;
--- Libs/can_al_freertos/can_al_freertos.h
+++ Libs/can_al_freertos/can_al_freertos.h
@@ -11,10 +11,12 @@
 #define CAN_TX_QUEUE_SIZE 16
 #define CAN_TX_TASK_STACK_SIZE 128
 
+#define CAN_OK 0
+
 extern CAN_HandleTypeDef hcan;
 
 typedef struct {
-    uint16_t            id;     //11-bit ID
+    uint16_t            id;      //11-bit ID
     uint8_t             len;     //length of message in bytes (0 - 8)
     uint8_t             data[8]; //data to send (up to 8 bytes)
 } CAN_msg_t;
@@ -29,9 +31,11 @@ typedef struct {
     TaskHandle_t        tx_task_handle;
 } CAN_al_t;
 
+extern CAN_al_t *can_ptr;
+
 typedef struct {
     CAN_HandleTypeDef*  hcan;
-    uint8_t*            filter_ids;
+    uint16_t*           filter_ids;
     uint16_t            node_id;
 } CAN_al_config;
 
@@ -42,10 +46,18 @@ typedef struct {
 /// @param filter_ids list of ids to enable receiving CAN messages from.
 /// @param num_of_ids number of ids in the list of filter_ids.
 /// @return 0 on success, else non-zero
-int CAN_al_init(CAN_al_t* can, CAN_al_config* config, uint8_t* filter_ids, int num_of_ids);
+int CAN_al_init(CAN_al_t* can, CAN_al_config* config, uint16_t* filter_ids, int num_of_ids);
+
+/// @brief Adds filters to CAN connection (discrete)
+///         Takes an array of 11-bit integers to use as filters for the CAN Controller
+/// @param hcan Pointer to CAN handle
+/// @param length Length of ids array
+/// @param ids Array of 11-bit CAN identifiers
+/// @return
+HAL_StatusTypeDef CAN_Add_Filter_Discrete(CAN_HandleTypeDef *hcan, int length, uint16_t *ids);
 
 // Sending data
-/// @brief Add message to CAN sending queue.
+/// @brief Add message to CAN send queue.
 /// @param can Pointer to CAN context
 /// @param msg message to send
 /// @return 0 if successful, else 1
@@ -59,7 +71,7 @@ int CAN_tx_enqueue(CAN_al_t* can, CAN_msg_t* msg);
 int CAN_rx_get_from_queue(CAN_al_t* can, CAN_msg_t* msg);
 
 // ISR Callback
-void HAL_CAN_RxEventCallback(CAN_HandleTypeDef* hcan, uint16_t size);
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan);
 
 // Send Task Creation
 /// @brief Transmit task that physically sends messages from tx_queue 
