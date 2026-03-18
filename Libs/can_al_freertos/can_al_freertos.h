e0da548cdb845ca5fa6cb9c8c65e067727c32e3c
Aidan <aidanlbaker23@gmail.com>
2026-03-18 13:56:22 -0400

    updates to can freertos library. Added filtering in init and implemented receive callback

--- Libs/can_al_freertos/can_al_freertos.h
+++ Libs/can_al_freertos/can_al_freertos.h
@@ -14,7 +14,7 @@
 extern CAN_HandleTypeDef hcan;
 
 typedef struct {
-    uint16_t            id;     //11-bit ID
+    uint16_t            id;      //11-bit ID
     uint8_t             len;     //length of message in bytes (0 - 8)
     uint8_t             data[8]; //data to send (up to 8 bytes)
 } CAN_msg_t;
@@ -42,10 +42,18 @@ typedef struct {
 /// @param filter_ids list of ids to enable receiving CAN messages from.
 /// @param num_of_ids number of ids in the list of filter_ids.
 /// @return 0 on success, else non-zero
-int CAN_al_init(CAN_al_t* can, CAN_al_config* config, uint8_t* filter_ids, int num_of_ids);
+int CAN_al_init(CAN_al_t* can, CAN_al_config* config, uint16_t* filter_ids, int num_of_ids);
+
+/// @brief Adds filters to CAN connection (discrete)
+///         Takes an array of 11-bit integers to use as filters for the CAN Controller
+/// @param hcan Pointer to CAN handle
+/// @param length Length of ids array
+/// @param ids Array of 11-bit CAN identifiers
+/// @return
+HAL_StatusTypeDef CAN_Add_Filter_Discrete(CAN_HandleTypeDef *hcan, int length, uint16_t *ids);
 
 // Sending data
-/// @brief Add message to CAN sending queue.
+/// @brief Add message to CAN send queue.
 /// @param can Pointer to CAN context
 /// @param msg message to send
 /// @return 0 if successful, else 1
@@ -59,7 +67,7 @@ int CAN_tx_enqueue(CAN_al_t* can, CAN_msg_t* msg);
 int CAN_rx_get_from_queue(CAN_al_t* can, CAN_msg_t* msg);
 
 // ISR Callback
-void HAL_CAN_RxEventCallback(CAN_HandleTypeDef* hcan, uint16_t size);
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef* hcan);
 
 // Send Task Creation
 /// @brief Transmit task that physically sends messages from tx_queue 
